9c06466b79c2029458eade9ced2d324e79c1091f
Paul Niklas Ruth <panikruth@gmail.com>
2024-10-21 16:38:35 +0100

    Fix pull script

--- pull_files_from_repo.py
+++ pull_files_from_repo.py
@@ -3,9 +3,8 @@ from urllib.request import urlopen
 import io
 
 if __name__ == '__main__':
-    REPO = "https://github.com/DurhamARC/BasicProgrammingPython"
-    RELEASE = "June2024"
-    url = f"{REPO}/archive/refs/tags/{RELEASE}.zip"
+    REPO = "https://github.com/DurhamARC-Training/BasicProgrammingPython"
+    url = f"{REPO}/zipball/main"
     print(f"Downloading {url}...)")
     with urlopen(url) as url_obj:
         zip_content = io.BytesIO(url_obj.read())
