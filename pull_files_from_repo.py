66daef80eddc16c388bcc59d4c3f34a10939db5c
Paul Niklas Ruth <panikruth@gmail.com>
2024-10-21 16:50:37 +0100

    Add comments to download script

--- pull_files_from_repo.py
+++ pull_files_from_repo.py
@@ -6,8 +6,11 @@ if __name__ == '__main__':
     REPO = "https://github.com/DurhamARC-Training/BasicProgrammingPython"
     url = f"{REPO}/zipball/main"
     print(f"Downloading {url}...)")
+    # urlopen allows us to access files on the internet
     with urlopen(url) as url_obj:
+        # BytesIO to store zip in memory instead of writing to file on disk
         zip_content = io.BytesIO(url_obj.read())
-
+    
+    # extract zip file content
     with zipfile.ZipFile(zip_content) as zfile:
         zfile.extractall(".")
